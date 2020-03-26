6f17e46d2f2d6e783d0a62aca38868ddff637d97
VBLab <58396766+VBLab@users.noreply.github.com>
2020-03-26 13:57:32 +0100

    travail du jour
    qq modifs

--- public/static/js/objects.js
+++ public/static/js/objects.js
@@ -6,7 +6,8 @@
  *    - charger les données des objets dans la table
  */
 function load_components(){
+    
     console.log("Chargement des données de la page");
     // Ajouter ici le code permettant de charger dynamiquement les éléments de la page
 }
-
+load_components();
