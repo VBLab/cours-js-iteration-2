cb08ef95c151ae3ac359dde82f010f03d314ff9c
VBLab <58396766+VBLab@users.noreply.github.com>
2020-03-27 13:33:27 +0100

    repo de ce matin

--- public/static/js/objects.js
+++ public/static/js/objects.js
@@ -5,24 +5,43 @@
  *    - charger la liste des objets depuis l'API
  *    - charger les données des objets dans la table
  */
-function load_components(){
-    
+function load_components() {
+
     console.log("Chargement des données de la page");
-    $.get("/objects", function(data, status) {
-        console.log(data);
-    // Ajouter ici le code permettant de charger dynamiquement les éléments de la page
-});
+    $.get("/objects", function (data) {
+        for (let p of data.objects) {
+            add_line_to_table(p);
+        }
+        //console.log(p);
+        // Ajouter ici le code permettant de charger dynamiquement les éléments de la page
+    });
 }
-function add_line_to_table() {
-let line = '<tr>\
-<th> OBJ_001 </th>\
-<td><image</td>\
-<td>description</td>\
-<td><input type="radio"></td>\
-<th style="width: 100px"><button class="btn-primary">Achetez moi !</button></th>\
-</tr>';
 
-document.getElementById('table_body').innerHTML +- line ;
+// function add_line_to_table() {
+// let line = '<tr>\
+// <th> OBJ_001 </th>\
+// <td><image</td>\
+// <td>description</td>\
+// <td><input type="radio"></td>\
+// <th style="width: 100px"><button class="btn-primary">Achetez moi !</button></th>\
+// </tr>';
 
-}
+// document.getElementById('table_body').innerHTML += line ;
+
+
+//}
 // $('#table_body').append(line); // jquerry
+
+function add_line_to_table(data) {               /// Modif add_line /ajout les données d'un objet à la ligne
+    let line = '<tr>\
+    <th> '+ data.serial + ' </th>\
+    <th><img style="max-width: 70%; height: 10%;"src="/static/images/'+ data.image + '"></th>\
+    <th>'+ data.description + '</th>\
+    <th><input type="radio"></th>\
+    <th style="width: 100px"><button class="btn-primary">Achetez moi !</button></th>\
+    </tr>';
+
+    document.getElementById('table_body').innerHTML += line;
+
+}
+
